9e90b9a128559c5e889d0d771d42a7c11aa9dd63
Kuldeep Singh <ksingh51@uic.edu>
2023-08-06 18:55:46 -0500

    Notification functionality added.

--- background.js
+++ background.js
@@ -18,6 +18,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
       // Add the latest job entry at the beginning of the array
       jobApplications.unshift(jobApplicationData);
+      showNotification("Job application data stored successfully");
 
       chrome.storage.local.set({ jobApplications }, () => {
         if (chrome.runtime.lastError) {
@@ -32,5 +33,18 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 // Return true to indicate the response will be sent asynchronously
 return true;});
 
-  
-// });
+// Function to show a notification
+function showNotification(message) {
+  // Change the extension icon to a custom notification icon
+  chrome.action.setIcon({ path: "/assets/notif.png" });
+
+  // Set the icon back to the original after a few seconds (adjust the time as needed)
+  setTimeout(() => {
+    chrome.action.setIcon({ path: {
+      "16": "/assets/icon16.png",
+      "48": "/assets/icon48.png",
+      "128": "/assets/icon128.png",
+      "256": "/assets/icon256.png"
+    } });
+  }, 5000); // Display the custom icon for 3 seconds (you can change the time as needed)
+}
